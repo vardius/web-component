5db2cd79bad93c6d2ca2ae6e6f269bcc4a3383aa
Rafał Lorenz <vardius@gmail.com>
2017-05-29 08:55:03 +1000

    Dependency update

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,36 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify';
+import nodeResolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import replace from "rollup-plugin-replace";
+import uglify from "rollup-plugin-uglify";
 
-var env = process.env.NODE_ENV
+var env = process.env.NODE_ENV;
 var config = {
-  format: 'umd',
-  moduleName: 'web-component',
+  format: "umd",
+  moduleName: "web-component",
   plugins: [
     nodeResolve({
       jsnext: true
     }),
     babel({
-      exclude: 'node_modules/**'
+      exclude: "node_modules/**"
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify(env)
+      "process.env.NODE_ENV": JSON.stringify(env)
     })
   ]
-}
+};
 
-if (env === 'production') {
+if (env === "production") {
   config.plugins.push(
     uglify({
       compress: {
         pure_getters: true,
         unsafe: true,
         unsafe_comps: true,
-        warnings: false,
-        screw_ie8: false
-      },
-      mangle: {
-        screw_ie8: false
-      },
-      output: {
-        screw_ie8: false
+        warnings: false
       }
     })
-  )
+  );
 }
 
-export default config
+export default config;
