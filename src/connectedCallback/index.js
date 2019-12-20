e57a218c03fd70c8a577ae65f038de25303c4d69
Glen Hughes <glen.866@gmail.com>
2019-12-20 15:50:49 +0000

    Allow mode to be changed

--- src/connectedCallback/index.js
+++ src/connectedCallback/index.js
@@ -10,7 +10,7 @@ export default options => original => {
       let shadowRoot = target.shadowRoot;
       if (!shadowRoot) {
         shadowRoot = target.attachShadow({
-          mode: "open"
+          mode: options.mode ? options.mode : "open"
         });
       }
       target = shadowRoot;
